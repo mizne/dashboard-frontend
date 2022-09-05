e90ca81da7e585d637e3c98944eda7be7fd75b80
mizne <w20054319@126.com>
2022-09-06 07:07:32 +0800

    add smart chart

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,5 +1,6 @@
 import { LinkableComponent } from './linkable/linkable.component';
+import { SmallChartComponent } from './small-chart/small-chart.component';
 
-export const components = [LinkableComponent];
+export const components = [LinkableComponent, SmallChartComponent];
 
-export { LinkableComponent };
+export { LinkableComponent, SmallChartComponent };
---
+++ src/app/shared/components/small-chart/small-chart.component.ts
@@ -0,0 +1,43 @@
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import * as uuid from 'uuid';
+import { Chart } from '@antv/g2';
+
+@Component({
+  selector: 'small-chart',
+  templateUrl: './small-chart.component.html',
+})
+export class SmallChartComponent implements OnInit, AfterViewInit {
+  smallChartID = 'small-chart-container-' + uuid.v4();
+
+  @Input() data: number[] = [];
+  @Input() type: 'line' | 'bar' = 'line';
+  @Input() height = 40;
+  @Input() width = 120;
+
+  constructor() {}
+  ngOnInit() {}
+
+  ngAfterViewInit(): void {
+    const chart = new Chart({
+      container: this.smallChartID,
+      autoFit: false,
+      height: this.height,
+      width: this.width,
+    });
+    chart.data(this.data.map((e, i) => ({ value: e, index: i })));
+
+    if (this.type === 'line') {
+      chart.line().position('index*value');
+    } else if (this.type === 'bar') {
+      chart.interval().position('index*value');
+    } else {
+      console.warn(`[small-chart.component] unknown chart type: ${this.type}`);
+      chart.line().position('index*value');
+    }
+    chart.axis('index', false);
+    chart.axis('value', false);
+    chart.tooltip(false);
+
+    chart.render();
+  }
+}
