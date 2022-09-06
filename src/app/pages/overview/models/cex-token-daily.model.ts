3b901116f8f48a136d17512e7ad79ee0f2cb9955
mizne <w20054319@126.com>
2022-09-06 11:08:22 +0800

    remove unnecessary code

--- src/app/pages/overview/models/cex-token-daily.model.ts
+++ src/app/pages/overview/models/cex-token-daily.model.ts
@@ -19,16 +19,8 @@ export interface CexTokenDaily {
   readonly price: number;
   readonly volume: number;
 
-  readonly priceChange1Interval: number; // 价格变化 1周期
-  readonly priceChange7Interval: number; // 价格变化 7周期
-  readonly priceChange30Interval: number; // 价格变化 30周期
-
   readonly prices60Interval: number[]; // 价格序列 60周期
   readonly volumes60Interval: number[]; // 交易量序列 60周期
-
-  readonly priceChange1IntervalRelativeBTC: number; // 价格变化 相对btc价格变化 1周期
-  readonly priceChange7IntervalRelativeBTC: number; // 价格变化 相对btc价格变化 7周期
-  readonly priceChange30IntervalRelativeBTC: number; // 价格变化 相对btc价格变化 30周期
 
   readonly closeDeltaEma21: number; // 当前收盘价相对ema21
   readonly ema21DeltaEma55: number; // ema21相对ema55
