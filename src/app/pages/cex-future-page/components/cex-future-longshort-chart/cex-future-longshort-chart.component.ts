ca4810bbd6b76433cd873b2f2d0c36adbbd201a4
mizne <w20054319@126.com>
2023-04-14 17:25:14 +0800

    fix chart color

--- src/app/pages/cex-future-page/components/cex-future-longshort-chart/cex-future-longshort-chart.component.ts
+++ src/app/pages/cex-future-page/components/cex-future-longshort-chart/cex-future-longshort-chart.component.ts
@@ -138,7 +138,7 @@ export class CexFutureLongshortChartComponent implements OnInit, AfterViewInit {
       .interval()
       .adjust('stack')
       .position('time*value')
-      .color('type', ['red', '#ef6548', '#74c476', 'green']);
+      .color('type', ['red', '#ef6548', 'rgb(0, 109, 44)', 'rgb(0, 68, 27)']);
 
     chart.render();
   }
