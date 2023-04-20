032ba64ab505f88baaf007777a7d767575cba84a
mizne <w20054319@126.com>
2023-04-21 07:33:45 +0800

    fix githook script template

--- src/app/modules/create-notify-observer/components/timer/timer.component.ts
+++ src/app/modules/create-notify-observer/components/timer/timer.component.ts
@@ -77,9 +77,10 @@ const cheerio = require('cheerio');
 const logger = require('logger');
 const axios = require('axios');
 const fetchGitbookPages = require('fetchGitbookPages');
+const reportProgress = require('reportProgress');
 
 module.exports = async function parseData() {
-  const pages = await fetchGitbookPages('https://docs.alpacafinance.org/', 'https://docs.alpacafinance.org/past-products/stronk-vault')
+  const pages = await fetchGitbookPages('https://docs.alpacafinance.org/', 'https://docs.alpacafinance.org/past-products/stronk-vault', reportProgress)
 
   const pageFilter = (page) => {
     return page.lastUpdatedAt >= new Date().getTime() - 1 * 24 * 60 * 60 * 1e3
