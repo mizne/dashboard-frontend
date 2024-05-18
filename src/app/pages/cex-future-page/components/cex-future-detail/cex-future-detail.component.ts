d92561216946b6816664155d4b951e95bd60923f
mizne <w20054319@126.com>
2024-05-18 14:17:22 +0800

    fix chart style

--- src/app/pages/cex-future-page/components/cex-future-detail/cex-future-detail.component.ts
+++ src/app/pages/cex-future-page/components/cex-future-detail/cex-future-detail.component.ts
@@ -37,6 +37,23 @@ export class CexFutureDetailComponent implements OnInit {
       },
     },
   }
+
+  priceChartOptions = {
+    localization: {
+      priceFormatter: (n: number) => {
+        if (n >= 1e9) {
+          return `${(n / 1e9).toFixed(2)} B`
+        }
+        if (n >= 1e6) {
+          return `${(n / 1e6).toFixed(2)} M`
+        }
+        if (n >= 1e3) {
+          return `${(n / 1e3).toFixed(2)} K`
+        }
+        return `${(n).toFixed(2)}`
+      },
+    },
+  }
   priceSeries: Array<{
     type: string;
     color: string;
