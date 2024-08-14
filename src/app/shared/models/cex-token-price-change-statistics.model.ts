021a732113d2092026bb1e259782bd7d479b68a0
mizne <w20054319@126.com>
2024-08-14 19:27:40 +0800

    support cex token price change statistics ranking

--- src/app/shared/models/cex-token-price-change-statistics.model.ts
+++ src/app/shared/models/cex-token-price-change-statistics.model.ts
@@ -2,9 +2,11 @@ export interface CexTokenPriceChangeStatistics {
   readonly _id: string;
   readonly inDays: number; // 多少天内
 
-  readonly avgCurrentPriceRelative: number // 所有标的 的平均 当前价位
+  readonly avgCurrentPriceRelative: number // 所有标的 的平均当前价位
+  readonly avgCurrentPriceRelativeRanking: number // 所有标的 的平均当前价位 排行, 数值越小 表示平均当前价位越低
 
-  readonly avgPriceChangePercent: number; // 所有标的 的平均 涨跌幅
+  readonly avgPriceChangePercent: number; // 所有标的 的平均涨跌幅
+  readonly avgPriceChangePercentRanking: number; // 所有标的 的平均涨跌幅 排行, 数值越小 表示平均涨跌幅越低
 
   readonly time: number;
   readonly timeStr: string;
