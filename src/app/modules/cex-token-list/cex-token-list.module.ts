9996d6c015fa7a4492a2c1a4e2eb2a3cbb48e936
mizne <w20054319@126.com>
2025-04-20 12:25:14 +0800

    support view cex future detail

--- src/app/modules/cex-token-list/cex-token-list.module.ts
+++ src/app/modules/cex-token-list/cex-token-list.module.ts
@@ -3,12 +3,14 @@ import { SharedModule } from 'src/app/shared';
 import { CexTokenListComponent } from './cex-token-list.component'
 import { CexTokenSymbolItemModule } from 'src/app/modules/cex-token-symbol-item'
 import { CexTokenItemDetailModule } from 'src/app/modules/cex-token-item-detail'
+import { CexFutureItemDetailModule } from 'src/app/modules/cex-future-item-detail'
 
 @NgModule({
   imports: [
     SharedModule,
     CexTokenSymbolItemModule,
-    CexTokenItemDetailModule
+    CexTokenItemDetailModule,
+    CexFutureItemDetailModule,
   ],
   exports: [CexTokenListComponent],
   declarations: [CexTokenListComponent],
